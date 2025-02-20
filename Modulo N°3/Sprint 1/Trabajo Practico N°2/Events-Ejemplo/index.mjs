df88f6617e9f81d9aa0b18bf41cb156ebfbcdf7c
Sabino Felipe Tartalos Aredez <sabinoftaredez@gmail.com>
2025-02-20 16:19:16 -0300

    Eliminar archivos

--- Modulo N°3/Sprint 1/Trabajo Practico N°2/Events-Ejemplo/index.mjs
+++
@@ -1,9 +0,0 @@
-import {EventEmitter} from 'events';
-// Crear una Instancia de EventEmitter
-const emisor = new EventEmitter();
-// Definir un evento Personalizado
-emisor.on('Saludo', () => {
-    console.log('Hola, ${nombre}!');
-});
-// Emitir un evento de Hola Mundo
-emisor.emit('Saludo', 'Mundo');
