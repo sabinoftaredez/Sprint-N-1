bf4f520de5f4660f075dda47c3f0dbd1b8614156
sabinoftaredez <sabinoftaredez@gmail.com>
2025-02-20 20:21:52 -0300

    Eliminar TP

--- TPN4/proyect-root/Repository/superhéroesDataSource.mjs
+++
@@ -1,5 +0,0 @@
-export default class SuperhéroesDataSource {
-    obtenerTodos() {
-        throw new Error('Método debe ser implementado por la subclase');
-    }
-}
--- TPN4/proyect-root/View/superhéroesView.mjs
+++
@@ -1,6 +0,0 @@
-export function renderizarSuperhéroes(superhéroes) {
-    return JSON.stringify(superhéroes, null, 2);
-}
-export function renderizarListaSuperhéroes(superhéroes) {
-    return JSON.stringify(superhéroes, null, 2);
-}
